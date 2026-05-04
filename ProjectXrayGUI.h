fe87a1d3cddd634421805ffc0519c1a08c88c08f
guustnelissen <guust.nelissen@gmail.com>
2026-05-04 11:45:17 +0200

    Update ProjectXrayGUI.h

--- ProjectXrayGUI.h
+++ ProjectXrayGUI.h
@@ -1,34 +1,29 @@
 #pragma once
-
 #include <QtWidgets/QMainWindow>
-#include "ui_QT_basics.h"
-#include <onnxruntime_cxx_api.h> // Voor het AI model
-#include <opencv2/opencv.hpp>    // Voor het inladen van de foto
+#include "ui_ProjectXrayGUI.h"
+#include <onnxruntime_cxx_api.h>
+#include <opencv2/opencv.hpp>
 
-class QT_basics : public QMainWindow
+class ProjectXrayGUI : public QMainWindow
 {
 	Q_OBJECT
-
 public:
-	QT_basics(QWidget *parent = Q_NULLPTR);
-	~QT_basics(); // Destructor om het model netjes af te sluiten
+	ProjectXrayGUI(QWidget *parent = Q_NULLPTR);
+	~ProjectXrayGUI();
 
 private slots:
 	void on_btnScan_clicked();
 	void on_btnAnalyse_clicked();
-	void on_sbLeeftijd_valueChanged(int value); // Als de leeftijd verandert
+	void on_sbLeeftijd_valueChanged(int value);
 
 private:
-	Ui::QT_basicsClass ui;
+	Ui::ProjectXrayGUIClass ui;
 
-	// AI & ONNX Variabelen
 	Ort::Env env;
 	Ort::Session* session;
 
-	// Opslag voor de foto en het resultaat
 	QString currentImagePath;
 	QString lastLabel;
 
-	// Helper functie voor de overlevingskans
 	QString getSurvivalChance(const std::string& label, int age);
 };
