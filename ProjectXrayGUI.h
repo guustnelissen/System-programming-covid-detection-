b42dedb0bebefcaaf4937dca9f23585e79bfd82a
LucienHebbelynck <lucien.paul.f.hebbelynck@vub.be>
2026-05-04 10:38:29 +0200

    Update ProjectXrayGUI.h

--- ProjectXrayGUI.h
+++ ProjectXrayGUI.h
@@ -1,41 +1,34 @@
 #pragma once
+
 #include <QtWidgets/QMainWindow>
-#include <QLabel>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QString>
-#include <QSpinBox>
-#include <QLabel>
-#include <onnxruntime_cxx_api.h>
-#include <opencv2/opencv.hpp>
-#include <string>
+#include "ui_QT_basics.h"
+#include <onnxruntime_cxx_api.h> // Voor het AI model
+#include <opencv2/opencv.hpp>    // Voor het inladen van de foto
 
+class QT_basics : public QMainWindow
+{
+	Q_OBJECT
 
-class ProjectXrayGUI : public QMainWindow {  //this is inheritance of a class
-	Q_OBJECT		// a Qt macro that makes signals and slots possible
-					// slots =  functions thats are linked with buttons
 public:
-	ProjectXrayGUI(QWidget* parent = nullptr);
-	~ProjectXrayGUI();
+	QT_basics(QWidget *parent = Q_NULLPTR);
+	~QT_basics(); // Destructor om het model netjes af te sluiten
 
 private slots:
-	void openImage();
-	void classify();
-	void updateSurvivalChance(); //so that the survival chance updates wheb you change the age
+	void on_btnScan_clicked();
+	void on_btnAnalyse_clicked();
+	void on_sbLeeftijd_valueChanged(int value); // Als de leeftijd verandert
 
 private:
-	QLabel* imageLabel;
-	QLabel* resultLabel;
-	QPushButton* openButton;
-	QPushButton* classifyButton;
-	QString currentImagePath;
-	QSpinBox* ageInput;
-	QLabel* ageLabel;
-	QLabel* survivalChanceLabel;
-	QString getSurvivalChance(const std::string& label, int age);
-	QString lastLabel;
+	Ui::QT_basicsClass ui;
 
+	// AI & ONNX Variabelen
 	Ort::Env env;
 	Ort::Session* session;
+
+	// Opslag voor de foto en het resultaat
+	QString currentImagePath;
+	QString lastLabel;
+
+	// Helper functie voor de overlevingskans
+	QString getSurvivalChance(const std::string& label, int age);
 };
