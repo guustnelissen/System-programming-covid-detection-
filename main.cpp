7ed9b648bd8701008c7c6ba43f101ff9683c2db7
guustnelissen <guust.nelissen@gmail.com>
2026-05-04 11:45:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "QT_basics.h"
+#include "ProjectXrayGUI.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QT_basics w;
+	ProjectXrayGUI w;
 	w.show();
 	return a.exec();
 }
